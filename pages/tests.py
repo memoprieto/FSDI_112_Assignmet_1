df4f08fc4e330f5dccba208e9fdb93f537221e60
memoprieto <memoprieto79@hotmail.com>
2022-08-14 21:38:49 -0700

    FSDI_112_Assignment_2

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "index.html")
+
+    def test_about_page_uses_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
+
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_home_page_contains_text(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Success!")
+
+    def test_about_page_contains_text(self):
+        response = self.client.get("/about/")
+        self.assertContains(response, "My name is Guillermo Prieto-Avalos")
+        
+
+
+
